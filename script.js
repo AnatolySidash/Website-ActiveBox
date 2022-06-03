3c176d1ac1e0b359a90b2230a7807aaa9aaf27b1
AnatolySidash <sidash85@yandex.ru>
2022-06-03 14:10:41 +0300

    ActiveBox Slider done

--- script.js
+++ script.js
@@ -33,10 +33,38 @@ $(function() {
         let elementId = $(this).data('scroll');
         let elementOffset = $(elementId).offset().top;
 
+        nav.removeClass("show");
+
         $("html, body").animate({
             scrollTop: elementOffset - 75
         }, 750);
 
     });
+
+    // Burder Menu
+
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    });
+
+
+    // Reviews Slider: https://kenwheeler.github.io/slick/
+
+    let slider = $("#reviewsSlider");
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+      });
+
 
 });
